2e1c74fbfcfd308fc3a453bd9ec3cec2c7dc7e14
Jeffmac <jeanfrancois.macresy@gmail.com>
2024-06-23 19:56:36 +0200

    doc: first simple version of the  SDK documentation

--- vikit/common/context_managers.py
+++ vikit/common/context_managers.py
@@ -4,9 +4,6 @@ import string
 from datetime import datetime
 import inspect
 
-
-import emoji
-
 from loguru import logger
 
 
@@ -84,12 +81,11 @@ class Step:
         self.emoji = emoji
 
     def __enter__(self):
-        _emoji = emoji.emojize(self.emoji)
-        print(f"{_emoji} {self.msg}... ", end="", flush=True)
+        # print(f"{_emoji} {self.msg}... ", end="", flush=True)
         return self
 
-    def __exit__(self, exc_type, _exc_val, _exc_tb):
-        if exc_type is None:
-            print(emoji.emojize(":check_mark:"), flush=True)
-        else:
-            print(emoji.emojize(":cross_mark:"), flush=True)
+    # def __exit__(self, exc_type, _exc_val, _exc_tb):
+    #     # if exc_type is None:
+    #     #     print(emoji.emojize(":check_mark:"), flush=True)
+    #     # else:
+    #     #     print(emoji.emojize(":cross_mark:"), flush=True)
