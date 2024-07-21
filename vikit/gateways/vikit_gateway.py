587a9e7f4091a5ccd80dc97e5e795def4cdabbaa
leclem <clementmoutet@gmail.com>
2024-07-21 15:39:25 +0200

    feat: gateway for image prompt

--- vikit/gateways/vikit_gateway.py
+++ vikit/gateways/vikit_gateway.py
@@ -1,5 +1,6 @@
 import os
 import subprocess
+import numpy as np
 
 from tenacity import retry, before_log, after_log, stop_after_attempt
 from loguru import logger
@@ -12,10 +13,8 @@ from vikit.common.config import get_nb_retries_http_calls
 import requests
 import base64
 import json
-from base64 import b64decode
 import io
 from PIL import Image
-
 
 os.environ["REPLICATE_API_TOKEN"] = get_replicate_api_token()
 vikit_api_key = get_vikit_api_token()
@@ -454,17 +453,19 @@ class VikitGateway(MLModelsGateway):
             },
         )
 
-        logger.debug(f"Resizing image for video generator")
-        #Convert result to Base64
+        logger.debug("Resizing image for video generator")
+        # Convert result to Base64
         buffer = io.BytesIO()
-        imgdata = base64.b64decode(output.json()['image'])
+        imgdata = base64.b64decode(output.json()["image"])
         img = Image.open(io.BytesIO(imgdata))
         new_img = img.resize((1024, 576))  # x, y
         new_img.save(buffer, format="PNG")
-        img_b64 = "data:image/png;base64," + base64.b64encode(buffer.getvalue()).decode('utf-8')
-        
-        logger.debug(f"Generating video from image")
-        #Ask for a video
+        img_b64 = "data:image/png;base64," + base64.b64encode(buffer.getvalue()).decode(
+            "utf-8"
+        )
+
+        logger.debug("Generating video from image")
+        # Ask for a video
         output = requests.post(
             "https://videho.replit.app/models",
             json={
@@ -532,3 +533,45 @@ class VikitGateway(MLModelsGateway):
         )
 
         return output.text
+
+    @retry(stop=stop_after_attempt(get_nb_retries_http_calls()), reraise=True)
+    def generate_video_from_image_Stability(self, prompt: np.ndarray):
+        """
+        Generate a video from the given image prompt
+
+        Args:
+            prompt: The image prompt to generate the video from
+
+        returns:
+                The link to the generated video
+        """
+
+        # TO DO: include camera motion parameters
+        # Convert image prompt to Base64
+        img = Image.fromarray(prompt)
+        new_img = img.resize((1024, 576))  # x, y
+        # Save the image to a buffer
+        buffer = io.BytesIO()
+        new_img.save(buffer, format="PNG")
+        # Encode the image as base64
+        img_b64 = "data:image/png;base64," + base64.b64encode(buffer.getvalue()).decode(
+            "utf-8"
+        )
+
+        logger.debug("Generating video from image prompt")
+        # Ask for a video
+        output = requests.post(
+            "https://videho.replit.app/models",
+            json={
+                "key": vikit_api_key,
+                "model": "stability_image2video",
+                "input": {
+                    "image": img_b64,
+                    "seed": 0,
+                    "cfg_scale": 1.8,
+                    "motion_bucket_id": 127,
+                },
+            },
+        )
+        print(output.json())
+        return "data:video/mp4;base64," + output.json()["video"]
