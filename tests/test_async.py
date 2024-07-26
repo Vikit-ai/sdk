60cd15168e13d50db5aaeed3042699bb05b58cea
Jeffmac <jeanfrancois.macresy@gmail.com>
2024-07-26 14:05:26 +0200

    test: improving async tests

--- tests/test_async.py
+++ tests/test_async.py
@@ -16,6 +16,16 @@ logger.add("log_test_async.txt", rotation="10 MB")
 class TestAsync:
 
     @pytest.mark.local_integration
+    def test_sinc_on_async_build_single_video_no_bg_music_without_subs(self):
+        with WorkingFolderContext():
+            video = RawTextBasedVideo("This is a prompt text")
+            built = video.build_non_async()
+
+            assert built.media_url is not None
+            assert os.path.exists(video.media_url), "The generated video does not exist"
+
+    @pytest.mark.local_integration
+    @pytest.mark.asyncio
     async def test_build_single_video_no_bg_music_without_subs(self):
         with WorkingFolderContext():
             video = RawTextBasedVideo("This is a prompt text")
