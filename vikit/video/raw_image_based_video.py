45f95521d6990c7fade6e5aea3ef0937c8fa7551
leclem <clementmoutet@gmail.com>
2024-07-30 00:26:53 +0200

    fix: no space in title for image prompt

--- vikit/video/raw_image_based_video.py
+++ vikit/video/raw_image_based_video.py
@@ -31,7 +31,7 @@ class RawImageBasedVideo(Video):
 
     def __init__(
         self,
-        title: str = "Image Prompt",
+        title: str = None,
         raw_image_prompt: str = None,
     ):
         """
@@ -51,7 +51,10 @@ class RawImageBasedVideo(Video):
 
         self._image = raw_image_prompt
         self._needs_reencoding = False
-        self._title = title
+        if title:
+            self._title = title
+        else:
+            self._title = "notitle"
         self.duration = 4.0
 
     @property
@@ -63,6 +66,19 @@ class RawImageBasedVideo(Video):
 
     @log_function_params
     def get_title(self):
+        if self._title:
+            return self._title
+        #  get the first and last words of the prompt
+        splitted_prompt = self._title.split(" ")
+        clean_title_words = [word for word in splitted_prompt if word.isalnum()]
+        if len(clean_title_words) == 1:
+            summarised_title = clean_title_words[0]
+        elif len(clean_title_words) > 1:
+            summarised_title = clean_title_words[0] + "-" + clean_title_words[-1]
+        else:
+            summarised_title = "ImagePrompt"
+        # Add a unique identifier suffix to prevent several videos having the same title in a composite down the road
+        self._title = summarised_title
         return self._title
 
     def get_duration(self):
