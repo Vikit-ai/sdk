4c016ef50b282b92955fe0422ffae6ed67219cf9
Clément Moutet <clementmoutet@gmail.com>
2024-12-26 12:04:31 +0100

    fix:reputting the script example as it was

--- script_example.py
+++ script_example.py
@@ -428,7 +428,7 @@ async def quality_check_with_gemini():
         vid_cp_final._is_root_video_composite = True
         for i in range(1, 2):
             current_image = str(i) + ".jpg"
-            prompt = await PromptFactory().create_prompt_from_multimodal_async(text=gemini_prompt,  image=current_image) #, reengineer_text_prompt_from_image_and_text=True
+            prompt = await PromptFactory().create_prompt_from_multimodal_async(text=gemini_prompt,  image=current_image, reengineer_text_prompt_from_image_and_text=True)
 
             # Query Gemini to get an appropriate prompt
             response = await ml_models_gateway.ask_gemini(prompt)
