efcfa8dd7d5e5412d943a3d8251603d038599af6
Clément Moutet <clementmoutet@gmail.com>
2025-02-03 22:51:40 +0100

    fix:tests working now

--- vikit/video/raw_multimodal_based_video.py
+++ vikit/video/raw_multimodal_based_video.py
@@ -71,8 +71,8 @@ class RawMultiModalBasedVideo(Video):
         """
         return str(VideoType.RAWMULTIMODAL)
 
-    def run_build_core_logic_hook(self, build_settings: VideoBuildSettings, ml_models_gateway, quality_check=None):
-        return super().run_build_core_logic_hook(build_settings, ml_models_gateway, quality_check)
+    def run_build_core_logic_hook(self, build_settings: VideoBuildSettings, ml_models_gateway):
+        return super().run_build_core_logic_hook(build_settings, ml_models_gateway)
 
     def get_core_handlers(self, build_settings) -> list[Handler]:
         """
